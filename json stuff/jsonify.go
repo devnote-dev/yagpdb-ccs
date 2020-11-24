4c5b52e6f15a580be3b23a120516d06cebfa44df
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2020-11-24 17:24:26 +0000

    Restructure, Flags & Compatibility
    Restructured / rewrote / reworked (etc) most of the code, it's more compact and efficient. Added the `-j` flag for JSON formatting, added more specific message type indications (should work with reply messages now). Also other small improvements I wont list here. :)

--- json stuff/jsonify.go
+++ json stuff/jsonify.go
@@ -9,58 +9,54 @@
     See README.md for more info on use.
 */}}
 
-{{$args := parseArgs 2 "Syntax is: `-json <#channel/ID> <messageID> [-file:flag]`" (carg "string" "channelID") (carg "int" "messageID") (carg "string" "file flag")}}
-{{$struct := "Message"}}{{$ver := "4.11"}}{{$c := ""}}{{$fcpt := false}}
-{{$channel := .Channel.ID}}{{$msglink := (print "https://discordapp.com/channels/" .Guild.ID "/" .Channel.ID "/" ($args.Get 1))}}
+{{$a := parseArgs 2 "```json <Channel:Mention> <messageID>\njson <Channel:Name> <messageID>\njson <Channel:ID> <messageID>\njson 0 <messageID>\n```**Optional Flags:**\n```\n[-j strict-format:flag]\n[-file/f attachment:flag]```"
+(carg "string" "chan") (carg "int" "msgID") (carg "string" "flag: -j | -f")}}
+{{$type := dict 0 "Default Message" 6 "Pinned Message" 7 "Join Message" 8 "Boost Message" 9 "Teir 1 Boost Message" 10 "Teir 2 Boost Message" 11 "Teir 3 Boost Message" 12 "Followed Channel Message" 19 "Reply Message"}}
+{{$struct := "Unknown"}}{{$ver := "JSONify v4.15"}}{{$fa := false}}{{$chan := .Channel}}
+{{$link := joinStr "/" "https://discordapp.com/channels" .Guild.ID .Channel.ID ($a.Get 1)}}{{$ctx := ""}}
 
-{{if $args}}
-    {{$l := sendMessageRetID nil (cembed "description" "Converting Message... <a:loading:760219029620523008>")}}
-    {{if not (eq ($args.Get 0) "0")}}
-        {{$channel = (getChannel (reReplace `<#|>` ($args.Get 0) "")).ID}}
-        {{$channel = (getChannel $channel).ID}}
-        {{$msglink = (print "https://discordapp.com/channels/" .Guild.ID "/" $channel "/" ($args.Get 1))}}
-    {{end}}
-    {{if ($msg := getMessage $channel ($args.Get 1))}}
-        {{with $msg}}
-            {{if .Embeds}}
-                {{$struct = (print (title (index .Embeds 0).Type) " Embed")}}
-            {{else if .Attachments}}
-                {{$struct = "Attachment Message"}}
-            {{else if eq .Type 6 7 8 9 10 11}}
-                {{$struct = (print "System Message: " .Type)}}
-            {{end}}
-        {{end}}
-        {{$time := div $msg.ID 4194304|mult 1000000|toDuration}}
-        {{$json := json $msg}}
-        {{if or (ge (len $json) 2048) (reFind `\[(?:{.*},?){4,}\]` $json)}}
-            {{$fcpt = true}}
-            {{$c = "The message you requested was either too big or contained something that would crash the CC. To prevent this, a downloadable attachment version will be sent instead."}}
-        {{else if ($args.IsSet 2)}}
-        {{if eq (str ($args.Get 2)) "-f" "-file"}}
-            {{$c = "The downloadable file attachment will be sent shortly. 👌"}}
-        {{end}}
-    {{end}}
-        {{if $fcpt}}
-            {{$f := (printf "REQUESTED: %s\nGUILD: %s - %d\nSNOWFLAKE: %s\nJSON:\n\n%s" currentTime .Guild.Name .Guild.ID $time $json)}}
-            {{sleep 1}}
-            {{deleteMessage nil $l 0}}
-            {{sendMessage nil (complexMessage "content" $c "file" $f)}}
-        {{else}}
-            {{$nembed := cembed
-                "author" (sdict "name" (print "Triggered by " .User.String) "icon_url" (.User.AvatarURL "256"))
-                "title" "JSON Output"
-                "description" (print "```" $json "```")
-                "fields" (cslice
-                    (sdict "name" "Channel" "value" (print "<#" $channel ">\n(ID " $channel ")") "inline" true)
-                    (sdict "name" "Message ID" "value" (print ($args.Get 1) "\n[Click here](" $msglink ") to go to message.") "inline" true)
-                    (sdict "name" "Message Type" "value" $struct "inline" true)
-                    (sdict "name" "Snowflake (Age)" "value" (humanizeDurationSeconds (currentTime.Sub ($time|.DiscordEpoch.Add))) "inline" true)
-                    (sdict "name" "Size" "value" (print (fdiv (len $json) 1000) "kb") "inline" true))
-                "footer" (sdict "text" (print "JSONify v" $ver))}}
-            {{sleep 1}}
-            {{editMessage nil $l (complexMessageEdit "embed" $nembed)}}
-        {{end}}
+{{$l := sendMessageRetID nil (cembed "description" "Converting Message... <a:loading:760219029620523008>")}}
+{{if ne ($a.Get 0) "0"}}
+    {{$chan = getChannel (or (reFind `\d+` ($a.Get 0)) ($a.Get 0))}}
+    {{$link = joinStr "/" "https://discordapp.com/channels" .Guild.ID $chan.ID ($a.Get 1)}}
+{{end}}
+{{if ($msg := getMessage $chan.ID ($a.Get 1))}}
+    {{with $msg.Embeds}}
+        {{$struct = print (title (index . 0).Type) " Embed"}}
     {{else}}
-        {{editMessage nil $l (complexMessageEdit "embed" (cembed "title" "Error" "description" "Unkown message. Please try again."))}}
+        {{$struct = or (and $msg.Attachments "Attachment Message") (and (eq $msg.Type 0 6 7 8 9 10 11 12 19) ($type.Get (toInt $msg.Type))) $struct}}
     {{end}}
+    {{$time := div $msg.ID 4194304|mult 1000000|toDuration}}
+    {{$json := json $msg}}
+    {{if ($a.IsSet 2)}}
+        {{if (reFind `-j` ($a.Get 2))}}
+            {{$json = reReplace `,` $json ",\n"}}
+        {{end}}
+        {{if (reFind `-f(?:ile)?` ($a.Get 2))}}
+            {{$ctx = "The downloadable file attachment will be sent shortly. 👌"}}{{$fa = true}}
+        {{end}}
+    {{end}}
+    {{if or (ge (len $json) 2048) (reFind `\[(?:{.*},?){4,}\]` $json)}}
+        {{$ctx = "The message you requested was either too big or contained something that would crash the CC. To prevent this, a downloadable attachment version will be sent instead."}}
+        {{$fa = true}}
+    {{end}}
+    {{if $fa}}
+        {{deleteMessage nil $l 0}}
+        {{sendMessage nil (complexMessage "content" $ctx "file" (printf "REQUESTED BY: %s (%d)\nDATE/TIME: %s\nGUILD: %s (%d)\nCHANNEL: %s (%d)\nSNOWFLAKE: %s\nJSON:\n\n%s" .User.String .User.ID currentTime .Guild.Name .Guild.ID .Channel.Name .Channel.ID $time $json))}}
+    {{else}}
+        {{$e := cembed
+            "author" (sdict "name" (print "Triggered by " .User.String) "icon_url" (.User.AvatarURL "256"))
+            "title" "JSON Output"
+            "description" (print "```json\n" $json "\n```")
+            "fields" (cslice
+                (sdict "name" "Channel" "value" (print "<#" $chan.ID ">\n(ID " $chan.ID ")") "inline" true)
+                (sdict "name" "Message ID" "value" (print ($a.Get 1) "\n[Click here](" $link ") to go to message.") "inline" true)
+                (sdict "name" "Message Type" "value" $struct "inline" true)
+                (sdict "name" "Snowflake (Age)" "value" (humanizeDurationSeconds (currentTime.Sub ($time|.DiscordEpoch.Add))) "inline" true)
+                (sdict "name" "Size" "value" (print (fdiv (len $json) 1000) "kb") "inline" true))
+            "footer" (sdict "text" $ver)}}
+        {{editMessage nil $l (complexMessageEdit "embed" $e)}}
+    {{end}}
+{{else}}
+    {{editMessage nil $l (complexMessageEdit "embed" (cembed "title" "Error" "description" "Unkown message. Please try again."))}}
 {{end}}
