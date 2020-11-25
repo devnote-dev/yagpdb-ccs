fd994d0696b6026d0b22c8127e0e25f7057423b2
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2020-11-25 22:45:59 +0000

    Create count-main.go

---
+++ counting/count-main.go
@@ -0,0 +1,46 @@
+{{/*
+    Counting Main CC
+
+    Made By Devonte#07545 / Naru#6203
+
+    Recommended Trigger Type: Regex
+    Recommended Trigger     : .*
+*/}}
+
+{{/* THINGS TO CHANGE */}}
+
+{{$roleID := 0}} {{/* Role ID of the role to give, leave as 0 for none */}}
+
+{{$tracker := false}} {{/* Individual user count tracker, change to "true" to enable */}}
+
+{{/* ACTUAL CODE - DO NOT TOUCH */}}
+
+{{if not (dbGet 5 "c_count")}}
+    {{dbSet 5 "c_count" "0"}}
+    {{dbSet 5 "c_counter" (str .User.ID)}}
+    {{$rm := "No count role has been set."}}{{if $roleID}}{{$rm = print "The count role has been set to <@&" $roleID ">. You will get this role after counting more than once."}}{{end}}
+    {{sendMessage nil (cembed
+        "title" "Welcome to Counting!"
+        "description" (printf "I see this is your first time using this CC! I have set the count to **0** and the user to **%s**! %s\nMake sure that this CC only runs in this channel (<#%d>), otherwise the CC will run in too many channels and break. Have fun!" .User.String $rm .Channel.ID)
+        "color" (randInt 111111 999999)
+        "footer" (sdict "text" "You can delete this message now :)"))}}
+{{else}}
+    {{$cUser := (toInt (dbGet 5 "c_counter").Value)}}
+    {{$cCount := (toInt (dbGet 5 "c_count").Value)}}
+    {{if ($n := reFind `\A\d+\z` (joinStr " " .Args))}}
+        {{if ne .User.ID $cUser}}
+            {{if eq (toInt $n) (add $cCount 1)}}
+                {{if $roleID}}
+                    {{takeRoleID $cUser $roleID}}{{addRoleID $roleID}}
+                {{end}}
+                {{dbSet 5 "c_count" $n}}
+                {{dbSet 5 "c_counter" (str .User.ID)}}
+                {{if $tracker}}
+                    {{$_ := dbIncr .User.ID "count_tracker" 1}}
+                {{end}}
+            {{else}}{{deleteTrigger 0}}{{end}}
+        {{else}}
+            {{deleteTrigger 0}}{{sendDM "You cant count twice in a row!"}}
+        {{end}}
+    {{else}}{{deleteTrigger 0}}{{end}}
+{{end}}
