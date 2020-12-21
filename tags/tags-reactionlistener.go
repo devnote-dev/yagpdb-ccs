b59f8cd1a4bbbd9e431e8a579d8fad9f4fb10b0c
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2020-12-21 00:59:38 +0000

    Create tags-reactionlistener.go

---
+++ tags/tags-reactionlistener.go
@@ -0,0 +1,55 @@
+{{/*
+    Tags ReactionListener CC
+
+    Made By Devonte#0745 / Naru#6203
+    Contributors: Maverick Wolf#1010
+
+    Recommended Trigger Type: Reaction (Added and Removed)
+
+*/}}
+
+{{if .ReactionAdded}}
+    {{if and .Message.Embeds (eq .Message.Author.ID 204255221017214977)}}
+        {{$msg := index .Message.Embeds 0|structToSdict}}
+        {{if (reFind `\ATag(?::.+|\sList|\sSearch Results|\sHelp)\b` $msg.Title)}}
+            {{range $k, $v := $msg}}
+                {{if eq (kindOf $v true) "struct"}}{{$msg.Set $k (structToSdict $v)}}{{end}}
+            {{end}}
+            {{if $msg.Footer}}
+                {{if and (eq .Reaction.Emoji.Name "📱") (reFind `React with 📱 to be DMed a mobile version\.\x{200b}` $msg.Footer.Text)}}
+                    {{sendDM (printf "\n**%s**\n%s\n%s\n%s" $msg.Title $msg.Description (or $msg.Image (sdict "URL" "")).URL (reReplace `\)\n.+` $msg.Footer.Text ")"))}}
+                    {{deleteAllMessageReactions nil .Message.ID}}{{addMessageReactions nil .Message.ID "📱"}}
+                {{else if and (eq .Reaction.Emoji.Name "🗑") (reFind `React with 🗑 to delete this message\.` $msg.Footer.Text)}}
+                    {{deleteAllMessageReactions nil .Message.ID}}
+                    {{deleteMessage nil .Message.ID 1}}
+                {{else if and (eq .Reaction.Emoji.Name "◀" "▶") (reFind `React with 🗑 to delete this message.\x{200b}\nPage: \d+` $msg.Footer.Text)}}
+                    {{$list := ""}}{{$skip := 0}}
+                    {{$page := (reFind `\d+` $msg.Footer.Text|toInt)}}
+                    {{if eq .Reaction.Emoji.Name "▶"}}
+                        {{if not (reFind `No Further Entries` $msg.Description)}}
+                            {{$msg.Footer.Set "text" (print "React with 🗑 to delete this message.\u200b\nPage: " (add $page 1))}}
+                            {{$skip = mult $page 10}}
+                            {{range (dbTopEntries `tag\_%` 10 $skip)}}{{$list = print $list "\n`" (slice .Key 4) "`"}}{{end}}
+                            {{if $list}}
+                                {{$msg.Set "description" $list}}
+                                {{editMessage nil .Message.ID (complexMessageEdit "embed" $msg)}}
+                                {{deleteMessageReaction nil .Message.ID .User.ID "▶"}}
+                            {{end}}
+                        {{end}}
+                    {{else if eq .Reaction.Emoji.Name "◀"}}
+                        {{if ne $page 1}}
+                            {{$msg.Footer.Set "text" (print "React with 🗑 to delete this message.\u200b\nPage: " (sub $page 1))}}
+                            {{$skip = div $page 10|roundCeil|toInt}}
+                            {{range (dbTopEntries `tag\_%` 10 $skip)}}{{$list = print $list "\n`" (slice .Key 4) "`"}}{{end}}
+                            {{if $list}}
+                                {{$msg.Set "description" $list}}
+                                {{editMessage nil .Message.ID (complexMessageEdit "embed" $msg)}}
+                                {{deleteMessageReaction nil .Message.ID .User.ID "◀"}}
+                            {{end}}
+                        {{end}}
+                    {{end}}
+                {{end}}
+            {{end}}
+        {{end}}
+    {{end}}
+{{end}}
