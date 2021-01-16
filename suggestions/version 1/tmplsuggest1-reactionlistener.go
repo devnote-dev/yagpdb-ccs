d06de73763438bc3ec228cb7725fcefc5b3f9d0d
Devonte <69095599+NaruDevnote@users.noreply.github.com>
2021-01-16 03:01:55 +0000

    Update tmplsuggest1-reactionlistener.go

--- suggestions/version 1/tmplsuggest1-reactionlistener.go
+++ suggestions/version 1/tmplsuggest1-reactionlistener.go
@@ -1,22 +1,25 @@
 {{/*
-    Template-Suggestions v1 ReactionListener CC
+    Suggestions v1 ReactionListener CC
 
     Made By Devonte#0745 / Naru#6203
 
-    Trigger Type: Reaction (Added and Removed)
+    Trigger Type: Reaction - Added and Removed
 
     See tmplsuggest-v1.md for more info
+
+    © NaruDevnote 2020-2021 (GNU GPL v3)
+    https://github.com/NaruDevnote/yagpdb-ccs
 */}}
 
 {{/* THINGS TO CHANGE */}}
 
-{{$suggest_channel := }} {{/* The ID of the suggestion channel */}}
+{{$suggest_channel := CHANNEL-ID}} {{/* The ID of the suggestion channel */}}
 
-{{$main_channel := }} {{/* The ID of the review channel */}}
+{{$main_channel := CHANNEL-ID}} {{/* The ID of the review channel */}}
 
 {{$cooldown := 120}} {{/* Suggestions quote cooldown (default is 2 minutes) */}}
 
-{{$staff := cslice ROLEID ROLEID}} {{/* The IDs of staff roles (requires at least one) */}}
+{{$staff := cslice ROLE-ID ROLE-ID}} {{/* The IDs of staff roles (requires at least one) */}}
 
 {{/* ACTUAL CODE - DO NOT TOUCH */}}
 
@@ -24,9 +27,7 @@
     {{if and (eq .Channel.ID $suggest_channel) .Message.Embeds (eq .Message.Author.ID 204255221017214977)}}
         {{$msg := index (getMessage $suggest_channel .Message.ID).Embeds 0|structToSdict}}
         {{if reFind `(?i)suggest(?:ion)?` $msg.Title}}
-            {{range $k, $v := $msg}}
-                {{if eq (kindOf $v true) "struct"}}{{$msg.Set $k (structToSdict $v)}}{{end}}
-            {{end}}
+            {{range $k, $v := $msg}}{{if eq (kindOf $v true) "struct"}}{{$msg.Set $k (structToSdict $v)}}{{end}}{{end}}
             {{$msg.Author.Set "icon_url" $msg.Author.IconURL}}
             {{if eq .Reaction.Emoji.Name "💬"}}
                 {{if not (dbGet .Message.ID "suggest_qcd")}}
@@ -39,37 +40,44 @@
                 {{end}}
             {{else if eq .Reaction.Emoji.Name "🛡"}}
                 {{$isStaff := false}}
-                {{range $staff}}
-                    {{if hasRoleID .}}{{$isStaff = true}}{{end}}
-                {{end}}
+                {{range $staff}}{{if hasRoleID .}}{{$isStaff = true}}{{end}}{{end}}
                 {{if $isStaff}}
                     {{dbSetExpire .Message.ID "final" (str .User.ID) 300}}
-                    {{addMessageReactions nil .Message.ID "❌"}}
+                    {{addMessageReactions nil .Message.ID ":nonegrey:796925441855586374"}}
+                {{else if (eq ($a := reFind `\d{17,19}` $msg.Footer.Text|toInt|userArg).ID .User.ID)}}
+                    {{dbSetExpire .Message.ID "delete" true 300}}
+                    {{addMessageReactions nil .Message.ID ":nonegrey:796925441855586374"}}
                 {{end}}
-            {{else if eq .Reaction.Emoji.Name "❌"}}
+            {{else if eq .Reaction.Emoji.ID 796925441855586374}}
                 {{with (dbGet .Message.ID "final")}}
                     {{if eq (toInt .Value) $.User.ID}}
-                        {{deleteMessageReaction nil $.Message.ID $.User.ID "❌"}}
-                        {{deleteMessageReaction nil $.Message.ID 204255221017214977 "❌"}}
+                        {{deleteMessageReaction nil $.Message.ID $.User.ID ":nonegrey:796925441855586374"}}
+                        {{deleteMessageReaction nil $.Message.ID 204255221017214977 ":nonegrey:796925441855586374"}}
                         {{dbDel $.Message.ID "final"}}
                     {{end}}
                 {{end}}
-            {{else if eq .Reaction.Emoji.ID 737036090510278716 737036123767046155}}
-                {{if (dbGet .Message.ID "final")}}
-                    {{if eq (toInt (dbGet .Message.ID "final").Value) .User.ID}}
-                        {{$auth := reFind `\d{17,19}` $msg.Footer.Text|toInt}}
+            {{else if eq .Reaction.Emoji.ID 796925441771438080 796925441490681889}}
+                {{if ($db := dbGet .Message.ID "final")}}
+                    {{if eq (toInt $db.Value) .User.ID}}
+                        {{$auth := reFind `\d{17,19}` $msg.Footer.Text|toInt|userArg}}
                         {{$total := dict "upvotes" 0 "downvotes" 0}}
                         {{range .Message.Reactions}}
-                            {{if eq .Emoji.ID 737036090510278716}}{{$total.Set "upvotes" .Count}}{{end}}
-                            {{if eq .Emoji.ID 737036123767046155}}{{$total.Set "downvotes" .Count}}{{end}}
+                        {{if eq .Emoji.ID 796925441771438080}}{{$total.Set "upvotes" .Count}}{{end}}
+                        {{if eq .Emoji.ID 796925441490681889}}{{$total.Set "downvotes" .Count}}{{end}}
                         {{end}}
-                        {{$msg.Set "fields" (cslice (sdict "name" "Final Count" "value" (print "<:check:737036090510278716> " $total.upvotes "\n<:cross:737036123767046155> " $total.downvotes) "inline" true) (sdict "name" "Staff Responsible" "value" (printf "• %s - %s\n• ID %d" .User.Mention .User.String .User.ID) "inline" true))}}
-                        {{if eq .Reaction.Emoji.ID 737036090510278716}}
-                            {{sendMessage $main_channel (complexMessage "content" (print "<@" $auth "> Your suggestion was approved!") "embed" (cembed $msg))}}
-                        {{else if eq .Reaction.Emoji.ID 737036123767046155}}
-                            {{sendMessage $main_channel (complexMessage "content" (print "<@" $auth "> Your suggestion was denied (see message below for reason)") "embed" (cembed $msg))}}
+                        {{$msg.Set "fields" (cslice (sdict "name" "Final Count" "value" (print "<:checkgreen:796925441771438080> " $total.upvotes "\n<:crossred:796925441490681889> " $total.downvotes) "inline" true) (sdict "name" "Staff Responsible" "value" (printf "• %s - %s\n• ID %d" .User.Mention .User.String .User.ID) "inline" true))}}
+                        {{if eq .Reaction.Emoji.ID 796925441771438080}}
+                        {{sendMessage $main_channel (complexMessage "content" (print $auth.Mention " Your suggestion was approved!") "embed" (cembed $msg))}}
+                        {{else if eq .Reaction.Emoji.ID 796925441490681889}}
+                        {{sendMessage $main_channel (complexMessage "content" (print $auth.Mention " Your suggestion was denied (see message below for reason)") "embed" (cembed $msg))}}
                         {{end}}
-                        {{deleteMessage $suggest_channel .Message.ID 2}}
+                        {{deleteMessage $suggest_channel .Message.ID 1}}
+                    {{end}}
+                {{else if and (eq .Reaction.Emoji.ID 796925441490681889) (dbGet .Message.ID "delete")}}
+                    {{$auth := reFind `\d{17,19}` $msg.Footer.Text|toInt|userArg}}
+                    {{if eq $auth.ID .User.ID}}
+                        {{sendMessage $main_channel (cembed "description" (print .User.Mention " deleted their suggestion!\nSuggestion ID: " (reFind `\d+` $msg.Title)) "color" 0xff2e2e "timestamp" currentTime)}}
+                        {{deleteMessage $suggest_channel .Message.ID 1}}
                     {{end}}
                 {{end}}
             {{end}}
